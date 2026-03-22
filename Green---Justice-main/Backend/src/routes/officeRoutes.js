745c84a971308cd841169898d28bc38a9e2da80d
Sivaharan Mayoorthanan <sivamayoorthanan31@gmail.com>
2026-03-22 17:39:03 +0530

    Delete Green---Justice-main directory

--- Green---Justice-main/Backend/src/routes/officeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { protect } = require('../middlewares/authMiddleware');
-const { getSuggestedOffice } = require('../controllers/officeController');
-
-const router = express.Router();
-
-router.get('/suggest/:complaintId', protect, getSuggestedOffice);
-
-module.exports = router;
