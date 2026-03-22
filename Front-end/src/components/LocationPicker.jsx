f0489b0c7a6ca5220ba6ed6b82c48bdee18d34ed
Danustan <danudanukutty@gmail.com>
2026-03-22 14:25:47 +0530

    Update LocationPicker.jsx

--- Front-end/src/components/LocationPicker.jsx
+++ Front-end/src/components/LocationPicker.jsx
@@ -1,5 +1,30 @@
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import { useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+});
+
+
+const redIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 function LocationMarker({ setFormData, formData }) {
   const [position, setPosition] = useState(null);
@@ -7,6 +32,7 @@ function LocationMarker({ setFormData, formData }) {
   useMapEvents({
     click(e) {
       const { lat, lng } = e.latlng;
+
       setPosition([lat, lng]);
 
       setFormData({
@@ -17,7 +43,9 @@ function LocationMarker({ setFormData, formData }) {
     },
   });
 
-  return position === null ? null : <Marker position={position}></Marker>;
+  return position === null ? null : (
+    <Marker position={position} icon={redIcon} />
+  );
 }
 
 export default function LocationPicker({ formData, setFormData }) {
@@ -32,7 +60,10 @@ export default function LocationPicker({ formData, setFormData }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <LocationMarker formData={formData} setFormData={setFormData} />
+      <LocationMarker
+        formData={formData}
+        setFormData={setFormData}
+      />
     </MapContainer>
   );
 }
