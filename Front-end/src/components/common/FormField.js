2e390de044d8454850570b876102fd1a7a0a4b5c
Sivaharan Mayoorthanan <sivamayoorthanan31@gmail.com>
2026-04-30 15:13:28 +0530

    Add FormField component for form input handling

--- Front-end/src/components/common/FormField.js
+++ Front-end/src/components/common/FormField.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+export const FormField = ({ label, required, children }) => {
+    return (
+        <div className="form-group">
+            <label>
+                {label} {required && <span className="required">*</span>}
+            </label>
+            {children}
+        </div>
+    );
+};
