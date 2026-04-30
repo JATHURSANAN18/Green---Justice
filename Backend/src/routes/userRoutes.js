91e4a57146f0d191e8c5e86a28ce5d74ee5086dd
pathuman2004-ui <pathuman2004@gmail.com>
2026-04-30 16:15:24 +0530

    Remove user routes from userRoutes.js

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,9 +1,1 @@
-import express from 'express';
-import { registerUser, getUserInfo } from '../controllers/userController.js';
 
-const router = express.Router();
-
-router.post('/register', registerUser);
-router.get('/:id', getUserInfo);
-
-export default router;
